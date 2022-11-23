c96d5fbfb48576886a0a5fae8f50a5ad9fd45b8d
Artur Gontarczyk <gontarczyk.artur@gmail.com>
2022-11-23 01:43:56 +0100

    Update cap_file.py

--- tests/cap_file.py
+++ tests/cap_file.py
@@ -5,6 +5,8 @@ capabilities = {
     "browserName": "Firefox",
     "browserVersion": "106.0",
     "LT:Options": {
+      "username": os.getenv("LT_USERNAME"),
+      "accessKey": os.getenv("LT_ACCESS_KEY"),
       "platformName": "Windows 10",
       "project": "Untitled",
       "selenium_version": "4.0.0",
