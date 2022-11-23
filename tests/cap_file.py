8e5cf38edb0996cd45455424df098dc793d4806c
Artur Gontarczyk <gontarczyk.artur@gmail.com>
2022-11-23 13:44:09 +0100

    Update cap_file.py

--- tests/cap_file.py
+++ tests/cap_file.py
@@ -1,10 +1,10 @@
 import os
 
 capabilities = {
-	"browserName": os.getenv("LT_BROWSER_NAME"),
-	"browserVersion": os.getenv("LT_BROWSER_VERSION"),
+	"browserName": os.getenv("LT_BROWSER_NAME") or "Firefox",
+	"browserVersion": os.getenv("LT_BROWSER_VERSION") or "102.0",
 	"LT:Options": {
-		"platformName": os.getenv("LT_PLATFORM"),
+		"platformName": os.getenv("LT_PLATFORM") or "Windows 10",
 		"project": "Untitled",
 		"w3c": True
 	}
