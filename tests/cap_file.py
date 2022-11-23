e8dbc768c9c48f9c76852eabaa26187ddcfb08a0
Artur Gontarczyk <gontarczyk.artur@gmail.com>
2022-11-23 13:51:55 +0100

    Update cap_file.py

--- tests/cap_file.py
+++ tests/cap_file.py
@@ -3,7 +3,7 @@ import os
 browser_name: str = os.getenv("LT_BROWSER_NAME")
 
 capabilities = {
-	"browserName": browser_name,
+	"browserName": f"{browser_name}",
 	"browserVersion": "102.0",
 	"LT:Options": {
 		"platformName": "Windows 10",
