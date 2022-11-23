e40ed825a174b4785982ab8e6f44f2b854872b83
Artur Gontarczyk <gontarczyk.artur@gmail.com>
2022-11-23 01:46:37 +0100

    Update cap_file.py

--- tests/cap_file.py
+++ tests/cap_file.py
@@ -8,10 +8,7 @@ capabilities = {
       "username": os.getenv("LT_USERNAME"),
       "accessKey": os.getenv("LT_ACCESS_KEY"),
       "platformName": "Windows 10",
-      "project": "Untitled",
-      "selenium_version": "4.0.0",
-      "w3c": True,
-      "plugin": "python-pytest"
+      "project": "Untitled"
     }
   }
 }
