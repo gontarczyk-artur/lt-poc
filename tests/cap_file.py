0acf4ad58f6dc237461b8a512200bc10f0f0e07e
Artur Gontarczyk <gontarczyk.artur@gmail.com>
2022-11-23 02:14:23 +0100

    Update cap_file.py

--- tests/cap_file.py
+++ tests/cap_file.py
@@ -1,7 +1,12 @@
 capabilities = {
-    "browserName": "Firefox",
-    "browserVersion": "106.0",
-    "platformName": "Windows 10",
-    "project": "Untitled",
-    "selenium_version": "4.0.0"
+  "single_test": {
+	"browserName": "Chrome",
+	"browserVersion": "108.0",
+	"LT:Options": {
+		"platformName": "Windows 10",
+		"project": "Untitled",
+		"selenium_version": "4.0.0",
+		"w3c": True
+	}
+  }
 }
